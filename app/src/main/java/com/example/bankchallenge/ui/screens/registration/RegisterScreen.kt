35227b74b02e27386e47e50963afa06dc2ff2ea9
JPR <juanp_jpr@hotmail.com>
2024-04-28 17:08:17 -0300

    Add email password from register screen

--- app/src/main/java/com/example/bankchallenge/ui/screens/registration/RegisterScreen.kt
+++ app/src/main/java/com/example/bankchallenge/ui/screens/registration/RegisterScreen.kt
@@ -1,19 +1,40 @@
 package com.example.bankchallenge.ui.screens.registration
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CameraAlt
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.bankchallenge.R
+
 
 @Composable
 fun RegisterScreen(onRegistrationSuccess: () -> Unit) {
+    val viewModel = hiltViewModel<RegisterViewModel>()
+    val showPassword = remember { mutableStateOf(false) }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -21,10 +42,81 @@ fun RegisterScreen(onRegistrationSuccess: () -> Unit) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Registration Screen")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = onRegistrationSuccess) {
-            Text("Complete Registration")
+        TextField(
+            value = "",
+            onValueChange = { },
+            label = { Text("Name") },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Person,
+                    contentDescription = stringResource(R.string.login_email),
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
+        TextField(
+            value = "",
+            onValueChange = { },
+            label = { Text("Surname") },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Person,
+                    contentDescription = stringResource(R.string.login_email),
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
+        TextField(
+            value = viewModel.email.value,
+            onValueChange = { viewModel.onEmailChanged(it) },
+            label = { Text("Email") },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Email,
+                    contentDescription = stringResource(R.string.login_email),
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
+        viewModel.emailError.value?.let {
+            Text(text = stringResource(id = it), color = MaterialTheme.colorScheme.error)
+        }
+        TextField(
+            value = viewModel.password.value,
+            onValueChange = { viewModel.onPasswordChanged(it) },
+            leadingIcon = {
+                if (showPassword.value) {
+                    IconButton(onClick = { showPassword.value = false }) {
+                        Icon(
+                            imageVector = Icons.Filled.Visibility,
+                            contentDescription = stringResource(id = R.string.login_hide_password)
+                        )
+                    }
+                } else {
+                    IconButton(onClick = { showPassword.value = true }) {
+                        Icon(
+                            imageVector = Icons.Filled.VisibilityOff,
+                            contentDescription = stringResource(id = R.string.login_show_password)
+                        )
+                    }
+                }
+            },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = if (showPassword.value) VisualTransformation.None else PasswordVisualTransformation(),
+        )
+        viewModel.passwordError.value?.let {
+            Text(text = stringResource(id = it), color = MaterialTheme.colorScheme.error)
+        }
+        Button(onClick = { }) {
+            Icon(imageVector = Icons.Filled.CameraAlt, contentDescription = "foto")
+            Text("Tomar foto")
         }
     }
 }
--- app/src/main/java/com/example/bankchallenge/ui/screens/registration/RegisterViewModel.kt
+++ app/src/main/java/com/example/bankchallenge/ui/screens/registration/RegisterViewModel.kt
@@ -1,9 +1,61 @@
 package com.example.bankchallenge.ui.screens.registration
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.bankchallenge.R
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.util.regex.Pattern
 import javax.inject.Inject
 
 @HiltViewModel
-class RegisterViewModel @Inject constructor() : ViewModel(){
+class RegisterViewModel @Inject constructor() : ViewModel() {
+
+    private val emailPattern = Pattern.compile("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+")
+    private val passwordPattern =
+        Pattern.compile("^(?=.*[A-Z])(?=.*[!@#\$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?])(?=\\S+\$).{6,}\$")
+
+    private val _name = mutableStateOf("")
+    val name: State<String> = _name
+
+    private val _surname = mutableStateOf("")
+    val surname: State<String> = _surname
+
+    private val _email = mutableStateOf("")
+    val email: State<String> = _email
+
+    private val _password = mutableStateOf("")
+    val password: State<String> = _password
+
+    private val _emailError = mutableStateOf<Int?>(null)
+    val emailError: State<Int?> = _emailError
+
+    private val _passwordError = mutableStateOf<Int?>(null)
+    val passwordError: State<Int?> = _passwordError
+
+    fun onEmailChanged(email: String) {
+        _email.value = email
+        validateEmail(email)
+    }
+
+    fun onPasswordChanged(pass: String) {
+        _password.value = pass
+        validatePassword(pass)
+    }
+
+    private fun validateEmail(email: String) {
+        _emailError.value = if (email.isBlank()) {
+            R.string.error_email_empty
+        } else if (!emailPattern.matcher(email).matches()) {
+            R.string.error_email_format
+        } else null
+    }
+
+    private fun validatePassword(password: String) {
+        _passwordError.value = if (password.isBlank()) {
+            R.string.error_password_empty
+        } else if (!passwordPattern.matcher(password).matches()) {
+            R.string.error_password_format
+        } else null
+    }
 }

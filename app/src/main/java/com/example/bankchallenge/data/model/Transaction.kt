fce0296103a877d8273fc9548bc56583dc98848e
JPR <juanp_jpr@hotmail.com>
2024-04-27 21:26:00 -0300

    Delete unnecessary files

--- app/src/main/java/com/example/bankchallenge/data/model/Transaction.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bankchallenge.data.model
-
-data class Transaction(
-    val id: String,
-    val description: String,
-    val amount: Double,
-    val timestamp: Long
-)
--- app/src/main/java/com/example/bankchallenge/data/model/User.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bankchallenge.data.model
-
-data class User(
-    val id: String,
-    val name: String,
-    val lastName: String,
-    val balance: Double
-)
